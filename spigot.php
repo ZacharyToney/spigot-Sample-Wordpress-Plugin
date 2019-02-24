50127c327c87b90c7e467a99ee17eaaead390eee
Zachary Toney <zach@clearseries.com>
2019-02-24 15:05:26 -0500

    edit description
    Signed-off-by: Zachary Toney <zach@clearseries.com>

--- spigot.php
+++ spigot.php
@@ -6,7 +6,7 @@
 /*
 Plugin Name: Spigot Sample Wordpress Plugin
 Plugin URI: https://zachary.host/
-Description: This plugin injects a javascript snippet if it meets all criteria: 1. Android user agent 2. URL contains query string source with a value of google. It is then injected using jQuery and on document load waits 5 seconds, does an async call to a function called promptUser() which injects HTML after an element with i dof article, then writes the value of the source query string inside the element in "b" which I assume means bold tags.
+Description: This plugin injects a javascript snippet if it meets all criteria: 1. Android user agent 2. URL contains query string source with a value of google. It is then injected using jQuery and on document load waits 5 seconds, does an async call to a function called promptUser() which injects HTML after an element with the id of article, then writes the value of the source query string inside the element in "b" which I assume means bold tags from instructions.
 Author: Zachary Toney
 Version: 0.1
 Author URI: https://zachary.host/
